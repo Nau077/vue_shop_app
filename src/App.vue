611a2a67d9d6f4684a47a3e591421b001825083d
Nau077 <41802678+Nau077@users.noreply.github.com>
2019-03-27 13:22:40 +0300

    add first toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<v-app>
+    <v-navigation-drawer app temporary></v-navigation-drawer>
+
+    <v-toolbar app dark color="pramary">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Ad application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
